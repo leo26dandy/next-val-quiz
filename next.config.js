594a83f7608ea42225938c2e141a944089b0262b
Leo Dandy <leo26dandy@gmail.com>
2024-01-04 09:51:02 +0700

    delete empty folder

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
     domains: [
       'media.valorant-api.com'
     ],
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
   }
 }
 
