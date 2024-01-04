a213b40145495d9a7acac1970f1121e6f5ae755d
Leo Dandy <leo26dandy@gmail.com>
2024-01-04 13:48:12 +0700

    commenting fetch fn 3

--- app/page.jsx
+++ app/page.jsx
@@ -8,19 +8,19 @@ Renders a Next.js page component that displays a grid of character avatars with 
 import { Container } from '@/components'
 import Image from 'next/image'
 import Link from 'next/link'
-import { endpoint } from '@/utils/endpoint'
+// import { endpoint } from '@/utils/endpoint'
 
-async function getAllCharacters() {
-  const data = await fetch(`${endpoint}/characters`)
+// async function getAllCharacters() {
+//   const data = await fetch(`${endpoint}/characters`)
 
-  if (!data.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return data.json()
-}
+//   if (!data.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+//   return data.json()
+// }
 
 export default async function Page() {
-  const data = await getAllCharacters()
+  // const data = await getAllCharacters()
 
   return(
     <main>
