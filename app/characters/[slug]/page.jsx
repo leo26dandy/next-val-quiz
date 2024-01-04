c4979d1c37cb5b685127fa241730380d4dcd7815
Leo Dandy <leo26dandy@gmail.com>
2024-01-04 14:15:45 +0700

    uncommenting fn after 1st deployment vercel 5

--- app/characters/[slug]/page.jsx
+++ app/characters/[slug]/page.jsx
@@ -13,7 +13,7 @@ export async function generateStaticParams() {
 
 export async function getCharacterBySlug(slug) {
   try {
-    const data = await fetch(`https://next-val-quiz.vercel.app/api/characters/${slug}`);
+    const data = await fetch(`${process.env.apiEndpoint}/characters/${slug}`);
     
     if (!data.ok) {
       throw new Error(`Failed to fetch data: ${data.status} ${data.statusText}`);
